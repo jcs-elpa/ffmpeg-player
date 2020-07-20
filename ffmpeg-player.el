164fb15d70adbc186eb2d987f5c5143aa7336659
jenchieh <jcs090218@gmail.com>
2020-07-20 18:28:57 +0800

    Bump version.

--- ffmpeg-player.el
+++ ffmpeg-player.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Play video using ffmpeg.
 ;; Keyword: video ffmpeg buffering images
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Package-Requires: ((emacs "24.4") (s "1.12.0") (f "0.20.0"))
 ;; URL: https://github.com/jcs-elpa/ffmpeg-player
 
